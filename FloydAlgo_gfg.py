5f394f53d3a7037a88ed223e4e99f7c8d8c59431
EwanGray1 <161265972+EwanGray1@users.noreply.github.com>
2024-04-29 21:49:41 +0100

    Update FloydAlgo_gfg.py

--- FloydAlgo_gfg.py
+++ FloydAlgo_gfg.py
@@ -6,8 +6,10 @@ V = 4
 # Define infinity as a large enough value. This value will be used for vertices not connected to each other
 INF = 99999
 
+
 def floydWarshall(graph):
-    """dist[][] will be the output matrix that will finally have the shortest distances between every pair of vertices"""
+    """dist[][] will be the output matrix that will finally have the shortest distances
+    between every pair of vertices"""
     # initializing the solution matrix same as input graph matrix
     # OR we can say that the initial values of shortest distances are based on shortest paths considering no intermediate vertices
     dist = list(map(lambda i: list(map(lambda j: j, i)), graph))
@@ -22,6 +24,7 @@ def floydWarshall(graph):
                 dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
     return dist
 
+
 def printSolution(dist):
     print("Following matrix shows the shortest distances between every pair of vertices")
     for i in range(V):
@@ -32,3 +35,5 @@ def printSolution(dist):
                 print("%7d\t" % (dist[i][j]), end=' ')
             if j == V-1:
                 print()
+
+
